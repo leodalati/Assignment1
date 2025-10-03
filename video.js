ec5f7186be923d8d5fbbda42df1373c1967ad844
Leo Dalati <leo.dalati@ontariotechu.net>
2025-10-03 15:01:29 -0400

    added comments and fixed video

--- video.js
+++ video.js
@@ -1,5 +1,5 @@
-var myVideo=document.getElementById("video");
-  function playPause()
+var myVideo=document.getElementById("video"); //setting variable//
+  function playPause() //adding function for button//
 {
   if(myVideo.paused) myVideo.play();
   else myVideo.pause();
